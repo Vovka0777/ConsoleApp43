930540c5cba11d9c656cad26c3fa330f03b1becd
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-05-22 11:19:58 +0300

    Add files via upload

---
+++ Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43_2
+{
+    struct Notebook
+    {
+        public string model;
+        public string manufacturer;
+        public int price;
+
+        public Notebook(string model, string manufacturer, int price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Model: {model}, Manufacturer: {manufacturer}, Price: {price}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook1 = new Notebook("XPS", "Dell", 999);
+            notebook1.ShowInfo();
+            Console.ReadLine();
+        }
+    }
+}
